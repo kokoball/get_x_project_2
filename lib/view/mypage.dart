4b264d399adedc4353085f573351fc028f5ee854
kokoball <kbp4154@naver.com>
2023-07-03 22:47:20 +0900

    mypage gridview 추가

---
+++ lib/view/mypage.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chef Shop'),
+        backgroundColor: Colors.black87,
+        elevation: 0,
+        leading: const Icon(Icons.menu),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.view_list_rounded),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          ),
+        ],
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+            ),
+            itemBuilder: (context, index) {
+              return Container(
+                color: Colors.red,
+              );
+            },
+            itemCount: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
